b46e96601178bb0a6db8ca07bf20669b8c37ce88
Noël Widmer <noel.widmer@gmail.com>
2021-04-26 16:02:42 +0200

    Fixed the issue that score is not displayed during play

--- Assets/TatoUserInterface.cs
+++ Assets/TatoUserInterface.cs
@@ -8,18 +8,32 @@ public class TatoUserInterface : MonoBehaviour
     public Color Orange;
     public Color Red;
 
+    public Text ScoreLabel;
+    public Text ScoreValue;
+    public Color HighScoreColor;
+
     private Slider _slider;
     private Worm _worm;
+    private GameState _gameState;
 
     private void Start()
     {
         _worm = FindObjectOfType<Worm>();
         _slider = FindObjectOfType<Slider>();
+        _gameState = FindObjectOfType<GameState>();
     }
 
     private void LateUpdate()
     {
         ColoredBar.color = _worm.RemainingLifetimeFraction < .3f ? Red : _worm.RemainingLifetimeFraction < .7f ? Orange : Green;
         _slider.value = _worm.RemainingLifetimeFraction;
+
+        ScoreValue.text = _worm.Stars.ToString();
+
+        if (_worm.Stars > _gameState.HighScore)
+        {
+            ScoreLabel.text = "New High Score!";
+            ScoreValue.color = ScoreLabel.color = HighScoreColor;
+        }
     }
 }
